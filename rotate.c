1a423655e5dd73cbe28dbbec79504889943594ee
rootgopi <visitgopisankar@gmail.com>
2022-03-23 17:06:07 +0530

    version 2.0

--- rotate.c
+++ rotate.c
@@ -15,3 +15,35 @@ int main()
         rot--;
     }
   return 0;  
+
+  /*
+  #include<stdio.h>
+
+int main()
+{
+    int temp;
+    int arr[5]={1,2,3,4,5};
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<4;j++)
+        {
+            if(j!=0)
+             {
+            temp=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp;
+             }
+
+        }
+    }
+    for(int k=0;k<5;k++)
+    {
+        printf("%d\t",arr[k]);
+    }
+
+
+return 0;
+
+}
+
+*/
